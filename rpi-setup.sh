1314d98dc00bbc191c5184bde50f61c9c3c35ba8
Josh <josh4050@googlemail.com>
2022-04-14 15:01:58 +0100

    Hiding install output to make output more useful, debugging cp

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -12,14 +12,18 @@ echo -e "\nDevice is in room $ROOM with the identifier $IDENTIFIER"
 
 sed -i.bak -e "s/ROOM_HERE/$ROOM/" -e "s/IDENTIFIER_HERE/$IDENTIFIER/" 'raspberry-pi/server/app.py'
 
-apt-get install -y bluetooth libbluetooth-dev python-dev
+apt-get install -y bluetooth libbluetooth-dev python-dev > /dev/null
 cd 'raspberry-pi'
-pip install PyBluez
-python3 setup.py install
+pip install PyBluez > /dev/null
+python3 setup.py install > /dev/null
 cd ..
 
 sed -i.bak -e "s|PATH_TO_SERVER_FILE|$(pwd)/raspberry-pi/server/app.py|" 'raspberry-pi/systemctl/rpi-bluetooth-server.service'
-cp 'raspberry-pi/systemctl/rpi-bluetooth-server.service' '/etc/systemd/service'
+
+echo -e "cp \n"
+cp 'raspberry-pi/systemctl/rpi-bluetooth-server.service' '/etc/systemd/system'
+echo "$?"
+
 
 systemctl enable rpi-bluetooth-server.service
 systemctl start rpi-bluetooth-server.service
