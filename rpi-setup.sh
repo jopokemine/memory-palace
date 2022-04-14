6a98c5f518262bd8fb540a893292cd4c4e967fc4
Josh <josh4050@googlemail.com>
2022-04-14 15:09:55 +0100

    Added set -e and some useful logging to rpi-setup script

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [[ $EUID -ne 0 ]]; then
     sudo "$0" "$@"
@@ -8,22 +9,23 @@ fi
 read -p "What room will this device be in? " ROOM
 read -n 3 -p "Give this device a unique identifier (max. 3 characters): " IDENTIFIER
 
-echo -e "\nDevice is in room $ROOM with the identifier $IDENTIFIER"
+echo -e "\n\nDevice is in room $ROOM with the identifier $IDENTIFIER"
 
 sed -i.bak -e "s/ROOM_HERE/$ROOM/" -e "s/IDENTIFIER_HERE/$IDENTIFIER/" 'raspberry-pi/server/app.py'
 
-apt-get install -y bluetooth libbluetooth-dev python-dev > /dev/null
+echo -e '\nInstalling bluetooth libbluetooth-dev python-dev... '
+apt-get install -y bluetooth libbluetooth-dev python-dev > /dev/null && echo 'done'
+
 cd 'raspberry-pi'
-pip install PyBluez > /dev/null
-python3 setup.py install > /dev/null
+echo -e '\nInstalling PyBluez... '
+pip install PyBluez > /dev/null && echo 'done'
+echo -e '\nRunning setup.py install... '
+python3 setup.py install > /dev/null && echo 'done'
 cd ..
 
 sed -i.bak -e "s|PATH_TO_SERVER_FILE|$(pwd)/raspberry-pi/server/app.py|" 'raspberry-pi/systemctl/rpi-bluetooth-server.service'
 
-echo -e "cp \n"
 cp 'raspberry-pi/systemctl/rpi-bluetooth-server.service' '/etc/systemd/system'
-echo "$?"
-
 
 systemctl enable rpi-bluetooth-server.service
 systemctl start rpi-bluetooth-server.service
