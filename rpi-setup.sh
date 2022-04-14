296dcdc8ad9e4e9b38cdf8c851f7b91bb4e7198e
Josh <josh4050@googlemail.com>
2022-04-14 14:39:12 +0100

    Added service to auto start server, added more to rpi-setup script

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
 read -p "What room will this device be in? " ROOM
 read -n 3 -p "Give this device a unique identifier (max. 3 characters): " IDENTIFIER
 
 echo -e "\nDevice is in room $ROOM with the identifier $IDENTIFIER"
 
-sed -i '' -e "s/ROOM_HERE/$ROOM/" -e "s/IDENTIFIER_HERE/$IDENTIFIER/" 'raspberry-pi/examples/app.py'
+sed -i '' -e "s/ROOM_HERE/$ROOM/" -e "s/IDENTIFIER_HERE/$IDENTIFIER/" 'raspberry-pi/server/app.py'
 
-apt-get install -y bluetooth python-bluez
-
-cd raspberry-pi && python setup.py install
+apt-get install -y bluetooth libbluetooth-dev python-dev
+cd 'raspberry-pi'
+pip install PyBluez
+python setup.py install
 cd ..
+
+sed -i '' "s|PATH_TO_SERVER_FILE|$(pwd)/raspberry-pi/server/app.py|" 'raspberry-pi/systemctl/rpi-bluetooth-server.service'
+cp 'raspberry-pi/systemctl/rpi-bluetooth-server.service' '/etc/systemd/service'
+
+systemctl enable rpi-bluetooth-server.service
+systemctl start rpi-bluetooth-server.service
