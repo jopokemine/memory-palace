c0e01cb47897617006a275804e70fe2fcf462b9c
Josh <josh4050@googlemail.com>
2022-04-29 12:37:07 +0100

    Updated bluetooth boilerplate to remove unnecessary code and to add correct namespace

--- Memory-Palace/Assets/Scripts/Bluetooth/Bluetooth.cs
+++ Memory-Palace/Assets/Scripts/Bluetooth/Bluetooth.cs
@@ -1,50 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-
-public class Bluetooth : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        // A correct website page.
-        StartCoroutine(GetRequest("https://www.example.com"));
-
-        // A non-existing page.
-        StartCoroutine(GetRequest("https://error.html"));
-    }
-
-    IEnumerator GetRequest(string uri)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
-        {
-            // Request and wait for the desired page.
-            yield return webRequest.SendWebRequest();
-
-            string[] pages = uri.Split('/');
-            int page = pages.Length - 1;
-
-            switch (webRequest.result)
-            {
-                case UnityWebRequest.Result.ConnectionError:
-                case UnityWebRequest.Result.DataProcessingError:
-                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
-                    break;
-                case UnityWebRequest.Result.ProtocolError:
-                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
-                    break;
-                case UnityWebRequest.Result.Success:
-                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
-                    break;
-            }
+namespace MemoryPalace.Bluetooth {
+    public class Bluetooth : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+            
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-
+        // Update is called once per frame
+        void Update() {
+            
+        }
     }
 }
