38da5beb93664fe47b0975586b6026cbb6b4dcec
Josh <josh4050@googlemail.com>
2022-05-03 14:21:11 +0100

    Server now returns correct name for bluetooth device

--- raspberry-pi/server/app.py
+++ raspberry-pi/server/app.py
@@ -33,7 +33,7 @@ def get_bluetooth_rssi() -> object:
     rssi = "not in range" if rssi_vals.count(
         "not in range") >= 3 else average([val for val in rssi_vals if val != "not in range"])
 
-    return jsonify(name=f"mem_pal_{ROOM}_{UNIQUE_IDENTIFIER}", rssi=rssi)
+    return jsonify(name=f"mem-pal-{ROOM}-{UNIQUE_IDENTIFIER}", rssi=rssi)
 
 
 if __name__ == '__main__':
