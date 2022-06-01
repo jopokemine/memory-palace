d35b5b01c2bef20e642902a89aaf0c9fe8424aec
UP820734 <45854486+Josh-Danks@users.noreply.github.com>
2022-06-01 15:23:26 +0100

    Query Item Names

--- Memory-Palace/Assets/Scripts/Data Storage/Queries/Items.cs
+++ Memory-Palace/Assets/Scripts/Data Storage/Queries/Items.cs
@@ -29,5 +29,21 @@ namespace DataBank
             item.close();
             return myList;
         }
+
+        public List<string> getItems()
+        {
+            tbl_Item item = new tbl_Item();
+            System.Data.IDataReader reader = item.getItems();
+
+            int fieldCount = reader.FieldCount;
+		    List<string> myList = new List<string>();
+		    while (reader.Read())
+		    {
+                string entity = reader[0].ToString();
+                myList.Add(entity);
+		    }
+            item.close();
+            return myList;
+        }
     }
 }
